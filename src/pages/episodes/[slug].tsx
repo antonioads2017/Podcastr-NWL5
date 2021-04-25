4ca36a8782e34d89c73c90e5064f501d03863caa
Antonio Filho <antoniofilhonerde@gmail.com>
2021-04-25 18:08:29 -0300

    player funcional com funcao de play e pause

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import styles from './episode.module.scss';
+import { route } from 'next/dist/next-server/server/router';
 
 type Episode = {
   id: string;
@@ -57,8 +58,24 @@ export default function Episode({ episode }) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
